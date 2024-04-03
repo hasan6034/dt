74ea889775ff951a2340164b60a6ddf75c7dc1ac
atharvnegi <atharvnegi2951@gmail.com>
2024-04-03 22:13:43 +0530

    selene: Switch To HIDL memtrack
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -111,7 +111,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl
 
 PRODUCT_PACKAGES += \
     libdrm.vendor \
