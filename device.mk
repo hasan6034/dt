bab8eeec1060e99ec30918bfbebdf4c6c95db271
Hasan Basri OY <hasanbasrioy@gmail.com>
2025-07-19 13:53:29 +0300

    test

--- device.mk
+++ device.mk
@@ -408,7 +408,7 @@ PRODUCT_PACKAGES += \
     libui_shim
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
 # Recovery
 PRODUCT_COPY_FILES += \
